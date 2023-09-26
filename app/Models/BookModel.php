1b27d8b9962ed6185a5775b86facbd2360778bda
kesleymartins <kesley.martins@vivaldi.net>
2023-09-25 23:08:41 -0300

    create BookModel

---
+++ app/Models/BookModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\BookStatus;
+use CodeIgniter\Model;
+use App\Entities\Book;
+
+class BookModel extends Model
+{
+    protected $table            = 'books';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = Book::class;
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['title'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
