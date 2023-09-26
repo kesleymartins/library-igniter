aabaad4edad8109fe749bdbf602bc6a00c1d010f
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 11:01:46 -0300

    update indentation from 2 -> 4 spaces

--- app/Views/books/index.php
+++ app/Views/books/index.php
@@ -1,22 +1,23 @@
 <?= $this->extend('layouts/application.php') ?>
 
 <?= $this->section('content') ?>
-  <div class="pb-2 mb-4 border-bottom d-flex align-items-center justify-content-between">
-    <h2 class="h2">Livros</h2>
+    <div class="pb-2 mb-4 border-bottom d-flex align-items-center justify-content-between">
+        <h2 class="h2">Livros</h2>
 
-    <a href="#" class="btn btn-primary">Novo livro</a>
-  </div>
+        <a href="#" class="btn btn-primary">Novo livro</a>
+    </div>
 
-  <ul class="list-group ">
-    <?php foreach ($this->data as $book): ?>
-      <li class="list-group-item d-flex justify-content-between">
-        <span>
-          <?= $book->title ?>
-        </span>
-        <span>
-          <?= $book->statusName() ?>
-        </span>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+    <ul class="list-group ">
+        <?php foreach ($this->data as $book): ?>
+            <li class="list-group-item d-flex justify-content-between">
+                <span>
+                    <?= $book->title ?>
+                </span>
+
+                <span>
+                    <?= $book->statusName() ?>
+                </span>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 <?= $this->endSection() ?>
