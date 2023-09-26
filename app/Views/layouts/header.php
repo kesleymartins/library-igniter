e7be449e08f637e43be2e6e581463fb4af7758b9
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 17:15:22 -0300

    update link to books page

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-                <a href="#" class="nav-link px-2 link-body-emphasis">Books</a>
+                <?= anchor('books', 'Books', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
             </li>
         </ul>
     </div>
