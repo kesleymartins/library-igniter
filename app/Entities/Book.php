95e1f8d0fccf0b3d8cc1f7a56623fa9f7d2615f2
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 02:36:46 -0300

    create BookEntity method to fetch correctly status name

--- app/Entities/Book.php
+++ app/Entities/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Entities;
 
+use App\Enums\BookStatus;
 use CodeIgniter\Entity\Entity;
 
 class Book extends Entity
@@ -9,4 +10,11 @@ class Book extends Entity
     protected $datamap = [];
     protected $dates   = [];
     protected $casts   = [];
+
+    public function statusName()
+    {
+        $status = BookStatus::from($this->status);
+
+        return $status->name();
+    }
 }
