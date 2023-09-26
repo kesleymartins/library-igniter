1d4b3246737ea05e22b1e35806e88e1e03ae481d
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 10:57:57 -0300

    create a base header

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,9 @@
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+                <a href="#" class="nav-link px-2 link-body-emphasis">Books</a>
+            </li>
+        </ul>
+    </div>
+</header>
