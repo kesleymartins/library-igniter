2fab41ce6e039a16ded3d3c2a4f89f5f29048d19
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 23:38:18 -0300

    create form for books new/edit actions

---
+++ app/Views/books/_form.php
@@ -0,0 +1,13 @@
+<?= $this->include('shared/_form_errors') ?>
+
+<form method="post" action="/books" class="border rounded p-2">
+
+    <div class="mb-3">
+        <label for="title" class="form-label">Titulo</label>
+        <input type="text" name="title" id="title" class="form-control" value="<?= $book->title ?>"/>
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+        Salvar
+    </button>
+</form>
