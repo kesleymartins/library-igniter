9385f2e2447f95773f016ad3dc6359ec90d9c24d
kesleymartins <kesley.martins@vivaldi.net>
2023-09-27 20:20:27 -0300

    remove empty line

--- app/Views/books/_form.php
+++ app/Views/books/_form.php
@@ -1,7 +1,6 @@
 <?= $this->include('shared/_form_errors') ?>
 
 <form method="post" action="/books" class="border rounded p-2">
-
     <div class="mb-3">
         <label for="title" class="form-label">Titulo</label>
         <input type="text" name="title" id="title" class="form-control" value="<?= $book->title ?>"/>
