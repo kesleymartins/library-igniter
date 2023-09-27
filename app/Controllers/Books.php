73fb595de77596e636363e4e97128963c6b52875
kesleymartins <kesley.martins@vivaldi.net>
2023-09-27 20:16:24 -0300

    remove EOL empty char

--- app/Controllers/Books.php
+++ app/Controllers/Books.php
@@ -9,7 +9,7 @@ use CodeIgniter\HTTP\RedirectResponse;
 
 class Books extends BaseController
 {
-    public function index(): string 
+    public function index(): string
     {
         $bookModel = new BookModel();
         $books = $bookModel->findAll();
