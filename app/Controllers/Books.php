1952a6270f0237f6db289a7aa00448260277a93c
kesleymartins <kesley.martins@vivaldi.net>
2023-09-26 23:35:03 -0300

    add new/create actions

--- app/Controllers/Books.php
+++ app/Controllers/Books.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Entities\Book;
 use App\Models\BookModel;
+use CodeIgniter\HTTP\RedirectResponse;
 
 class Books extends BaseController
 {
@@ -13,5 +15,28 @@ class Books extends BaseController
         $books = $bookModel->findAll();
 
         return view('books/index', $books);
+    }
+
+    public function new(): string
+    {
+        $book = new Book();
+
+        return view('books/new', [
+            'book' => $book
+        ]);
+    }
+
+    public function create(): RedirectResponse 
+    {
+        $book = new Book($this->request->getPost());
+
+        $bookModel = new BookModel();
+
+        if ($bookModel->insert($book)) {
+            return redirect()->to('/books');
+        }
+
+        $errors = $bookModel->errors();
+        return redirect()->to('/books/new')->with('errors', $errors)->withInput();
     }
 }
