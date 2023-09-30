e75a4c7820e2863e6cd6714ba3330e6332238fd5
kesleymartins <kesley.martins@vivaldi.net>
2023-09-29 23:03:09 -0300

    add edit/update endpoints

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,5 @@ $routes->get('/', [Books::class, 'index']);
 $routes->get('/books', [Books::class, 'index']);
 $routes->get('/books/new', [Books::class, 'new']);
 $routes->post('/books', [Books::class, 'create']);
+$routes->get('/books/edit/(:num)', [Books::class, 'edit']);
+$routes->post('/books/(:num)', [Books::class, 'update']);
