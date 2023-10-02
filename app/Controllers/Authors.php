61a3ca7374c306f41b93d9b8092ba7c47fffdfd6
kesleymartins <kesley.martins@vivaldi.net>
2023-10-02 20:48:03 -0300

    create authors controller

---
+++ app/Controllers/Authors.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Entities\Author;
+use App\Models\AuthorModel;
+use CodeIgniter\HTTP\RedirectResponse;
+
+class Authors extends BaseController
+{
+}
