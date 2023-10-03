277d8bb21d3e380b5b417918a28089eb4f110482
kesleymartins <kesley.martins@vivaldi.net>
2023-10-03 06:56:55 -0300

    remove line with duplicated logic to open form

--- app/Views/books/_form.php
+++ app/Views/books/_form.php
@@ -3,7 +3,6 @@
 <?= $this->include('shared/_form_errors') ?>
 
 <?= form_with("/books", $book, ['class' => 'border rounded p-2']) ?>
-<form method="post" action="/books" class="border rounded p-2">
     <div class="mb-3">
         <label for="title" class="form-label">Titulo</label>
         <input type="text" name="title" id="title" class="form-control" value="<?= $book->title ?>"/>
