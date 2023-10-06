9944ab2b2fbaa2b60b9b20b65d3e22c0955a4328
kesleymartins <kesley.martins@vivaldi.net>
2023-10-05 21:42:24 -0300

    update Books index header title to Livros

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-                <?= anchor('books', 'Books', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
+                <?= anchor('books', 'Livros', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
             </li>
             <li>
                 <?= anchor('authors', 'Autores', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
