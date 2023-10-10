5bed5e8870ec4d6840bb46638f834963af8a63e0
kesleymartins <kesley.martins@vivaldi.net>
2023-10-09 23:28:21 -0300

    add method to return entity where book belongs

--- app/Entities/Book.php
+++ app/Entities/Book.php
@@ -4,6 +4,7 @@ namespace App\Entities;
 
 use App\Enums\BookStatus;
 use CodeIgniter\Entity\Entity;
+use App\Models\AuthorModel;
 
 class Book extends Entity
 {
@@ -22,4 +23,17 @@ class Book extends Entity
 
         return $status->name();
     }
+
+    /**
+     * Retorna a Entity do Autor em que o livro pertence
+     *
+     * @return Author
+     */
+    public function author(): Author
+    {
+        $authorModel = new AuthorModel();
+        $author = $authorModel->find($this->attributes['author_id']);
+
+        return $author;
+    } 
 }
