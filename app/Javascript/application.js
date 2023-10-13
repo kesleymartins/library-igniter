8404f8926138c481b02bec03d49b4d93aa4bbb9f
kesleymartins <kesley.martins@vivaldi.net>
2023-10-12 23:28:59 -0300

    setup header controller

--- app/Javascript/application.js
+++ app/Javascript/application.js
@@ -1,3 +1,5 @@
 import { Application } from "stimulus"
+import HeaderController from './controllers/header_controller'
 
 const application = Application.start()
+application.register('header', HeaderController)
