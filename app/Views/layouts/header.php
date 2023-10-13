8f2acd931b1ab5d3e8fbce7994e03e3aeac39d97
kesleymartins <kesley.martins@vivaldi.net>
2023-10-12 23:29:08 -0300

    implement header controller

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,11 +1,11 @@
-<header class="p-3 mb-3 border-bottom">
+<header class="p-3 mb-3 border-bottom shadow-sm" data-controller="header">
     <div class="container">
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-                <?= anchor('books', 'Livros', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
+                <?= anchor('books', 'Livros', ['id' => 'books', 'class' => 'nav-link px-2 link-body-emphasis']) ?>
             </li>
             <li>
-                <?= anchor('authors', 'Autores', ['class' => 'nav-link px-2 link-body-emphasis']) ?>
+                <?= anchor('authors', 'Autores', ['id' => 'authors', 'class' => 'nav-link px-2 link-body-emphasis']) ?>
             </li>
         </ul>
     </div>
